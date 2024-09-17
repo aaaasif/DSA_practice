53805bf41e3552f2811b5b7f38aae832c7714265
Abdullah Al Asif <alexasif4@gmail.com>
2024-09-17 23:17:52 +0800

    Encode Floating point

--- EnCode_FloatPoint.cpp
+++ EnCode_FloatPoint.cpp
@@ -3,3 +3,30 @@
 #include <bitset>
 #include <cstring>  // For memcpy
 
+// Function to print the IEEE 754 encoding of a float
+void printIEEE754(float number) {
+    // Create a union to access the bits of the float
+    union {
+        float f;
+        uint32_t bits;
+    } value;
+
+    value.f = number;
+
+
+}
+
+int main() {
+    float num1 = 56.8;
+    float num2 = -56.8;
+
+    std::cout << "Encoding of 56.8:" << std::endl;
+    printIEEE754(num1);
+
+    std::cout << std::endl;
+
+    std::cout << "Encoding of -56.8:" << std::endl;
+    printIEEE754(num2);
+
+    return 0;
+}
