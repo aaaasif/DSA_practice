6f4399963aaf6b6219ef3e2c3ca6e89b19af4507
Abdullah Al Asif <alexasif4@gmail.com>
2024-10-11 22:49:40 +0800

    Quene added

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,4 @@
-int main() {
-    float num1 = 56.8;
-    float num2 = -56.8;
 
-    std::cout << "Encoding of 56.8:" << std::endl;
-    printIEEE754(num1);
-
-    std::cout << std::endl;
-
-    std::cout << "Encoding of -56.8:" << std::endl;
-    printIEEE754(num2);
-
-    return 0;
 }
+
+int main() {
