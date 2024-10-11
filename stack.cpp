a3d85e27186cdf0146b69c79141a442660c283aa
Abdullah Al Asif <alexasif4@gmail.com>
2024-10-11 22:25:28 +0800

    List Added

---
+++ stack.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool checkPalindromeWithStack(const string& str) {
+    stack<char> charStack;
+    for (char ch : str) {
+        charStack.push(ch);
+    }
+
+    for (char ch : str) {
+        if (ch != charStack.top()) { 
+            return false;
+        charStack.pop(); 
+    }
+    return true;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (checkPalindromeWithStack(input)) {
+        cout << input << " is a palindrome." << endl;
+    } else {
+        cout << input << " is not a palindrome." << endl;
+    }
+    return 0;
+}
