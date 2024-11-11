0ea525974d50c8ed9a904d1a617af018dd797244
Abdullah Al Asif <abdullahalasif7910@gmail.com>
2024-11-11 15:09:17 +0800

    lab-2 added

--- dsaLab2.cpp
+++ dsaLab2.cpp
@@ -1,163 +1,191 @@
-#include <iostream>
-#include <algorithm>
-#include <stack>
-#include <chrono>
-#include <cstdlib>
-using namespace std;
+#include <bits/stdc++.h>
+    using namespace std;
 
-struct Comparator {
-    template <typename T>
-    static bool isLess(const T &a, const T &b) {
-        return a < b;
-    }
-};
-
-template <typename T>
-void swapElements(T arr[], int idx1, int idx2) {
-    T temp = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = temp;
-}
-
-template <typename T>
-int choosePivot(T arr[], int low, int high) {
-    return (low + high) / 2;
-}
-
-template <typename T, typename Cmp>
-int partitionArray(T arr[], int low, int high, T pivot) {
-    while (true) {
-        while (Cmp::isLess(arr[++low], pivot));
-        while (Cmp::isLess(pivot, arr[--high]));
-        if (low >= high) return low;
-        swapElements(arr, low, high);
-    }
-}
-
-template <typename T, typename Cmp>
-void quickSortClassic(T arr[], int low, int high) {
-    while (low < high) {
-        int pivotIdx = choosePivot(arr, low, high);
-        swapElements(arr, pivotIdx, high);
-        int partitionIdx = partitionArray<T, Cmp>(arr, low - 1, high + 1, arr[high]);
-        swapElements(arr, partitionIdx, high);
-
-        if (partitionIdx - low < high - partitionIdx) {
-            quickSortClassic<T, Cmp>(arr, low, partitionIdx - 1);
-            low = partitionIdx + 1;
-        } else {
-            quickSortClassic<T, Cmp>(arr, partitionIdx + 1, high);
-            high = partitionIdx - 1;
+    struct Ascending {
+        template <typename E>
+        static bool prior(const E &a, const E &b) {
+            return a < b;
         }
+    };
+
+    template <typename E>
+    void swap(E A[], int i, int j) {
+        E temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
-}
 
-template <typename T>
-int medianPivot(T arr[], int low, int high) {
-    int mid = (low + high) / 2;
-    if (arr[low] > arr[mid]) swapElements(arr, low, mid);
-    if (arr[low] > arr[high]) swapElements(arr, low, high);
-    if (arr[mid] > arr[high]) swapElements(arr, mid, high);
-    return mid;
-}
+    template <typename E>
+    inline int findpivot(E A[], int i, int j) {
+        return (i + j) / 2;
+    }
 
-template <typename T, typename Cmp>
-void hybridQuickSort(T arr[], int low, int high, const int threshold) {
-    while (low < high) {
-        if (high - low + 1 <= threshold) {
-            for (int i = low + 1; i <= high; i++) {
-                T temp = arr[i];
-                int j = i - 1;
-                while (j >= low && arr[j] > temp) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = temp;
+    template <typename E, typename Comp>
+    int partition(E A[], int left, int right, E &pivot) {
+        do {
+            while (Comp::prior(A[++left], pivot));
+            while ((left < right) && Comp::prior(pivot, A[--right]));
+            if (left < right) swap(A, left, right);
+        } while (left < right);
+        return left;
+    }
+
+    template <typename E, typename Comp>
+    void quicksortOriginal(E A[], int left, int right) {
+        while (left < right) {
+            int pivotIndex = findpivot(A, left, right);
+            swap(A, pivotIndex, right);
+            int partitionIndex = partition<E, Comp>(A, left - 1, right + 1, A[right]);
+            swap(A, partitionIndex, right);
+            
+            if (partitionIndex - left < right - partitionIndex) {
+                quicksortOriginal<E, Comp>(A, left, partitionIndex - 1);
+                left = partitionIndex + 1;
+            } else {
+                quicksortOriginal<E, Comp>(A, partitionIndex + 1, right);
+                right = partitionIndex - 1;
             }
-            return;
-        }
-        int pivotIdx = medianPivot(arr, low, high);
-        swapElements(arr, pivotIdx, high);
-        int partitionIdx = partitionArray<T, Cmp>(arr, low - 1, high + 1, arr[high]);
-        swapElements(arr, partitionIdx, high);
-
-        if (partitionIdx - low < high - partitionIdx) {
-            hybridQuickSort<T, Cmp>(arr, low, partitionIdx - 1, threshold);
-            low = partitionIdx + 1;
-        } else {
-            hybridQuickSort<T, Cmp>(arr, partitionIdx + 1, high, threshold);
-            high = partitionIdx - 1;
         }
     }
-}
 
-template <typename T, typename Cmp>
-void iterativeQuickSort(T arr[], int n, const int threshold) {
-    stack<pair<int, int>> ranges;
-    ranges.push({0, n - 1});
+    template <typename E>
+    int medianOfThreePivot(E A[], int left, int right) {
+        int mid = (left + right) / 2;
+        if (A[left] > A[mid]) swap(A, left, mid);
+        if (A[left] > A[right]) swap(A, left, right);
+        if (A[mid] > A[right]) swap(A, mid, right);
+        return mid;
+    }
 
-    while (!ranges.empty()) {
-        auto [low, high] = ranges.top();
-        ranges.pop();
+    template <typename E, typename Comp>
+    void quicksortMedian3(E A[], int left, int right) {
+        while (left < right) {
+            int pivotIndex = medianOfThreePivot(A, left, right);
+            swap(A, pivotIndex, right);
+            int partitionIndex = partition<E, Comp>(A, left - 1, right + 1, A[right]);
+            swap(A, partitionIndex, right);
 
-        if (high - low + 1 <= threshold) {
-            for (int i = low + 1; i <= high; i++) {
-                T temp = arr[i];
-                int j = i - 1;
-                while (j >= low && arr[j] > temp) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = temp;
+            if (partitionIndex - left < right - partitionIndex) {
+                quicksortMedian3<E, Comp>(A, left, partitionIndex - 1);
+                left = partitionIndex + 1;
+            } else {
+                quicksortMedian3<E, Comp>(A, partitionIndex + 1, right);
+                right = partitionIndex - 1;
             }
-            continue;
-        }
-
-        int pivotIdx = medianPivot(arr, low, high);
-        swapElements(arr, pivotIdx, high);
-        int partitionIdx = partitionArray<T, Cmp>(arr, low - 1, high + 1, arr[high]);
-        swapElements(arr, partitionIdx, high);
-
-        if (partitionIdx - low < high - partitionIdx) {
-            if (partitionIdx + 1 <= high) ranges.push({partitionIdx + 1, high});
-            if (partitionIdx - 1 >= low) ranges.push({low, partitionIdx - 1});
-        } else {
-            if (partitionIdx - 1 >= low) ranges.push({low, partitionIdx - 1});
-            if (partitionIdx + 1 <= high) ranges.push({partitionIdx + 1, high});
         }
     }
-}
 
-void measureSortingTime(void (*sortFunc)(int[], int, int, const int), int arr[], int n, const string &desc, int threshold) {
-    int *tempArr = new int[n];
-    copy(arr, arr + n, tempArr);
-
-    auto start = chrono::high_resolution_clock::now();
-    sortFunc(tempArr, 0, n - 1, threshold);
-    auto end = chrono::high_resolution_clock::now();
-
-    cout << desc << ": " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
-    delete[] tempArr;
-}
-
-int main() {
-    int n;
-    cout << "Enter number of elements (>= 1000): ";
-    cin >> n;
-
-    if (n < 1000) {
-        cout << "n must be >= 1000.\n";
-        return 1;
+    template <typename E>
+    void insertionSort(E A[], int left, int right) {
+        for (int i = left + 1; i <= right; ++i) {
+            E key = A[i];
+            int j = i - 1;
+            while (j >= left && A[j] > key) {
+                A[j + 1] = A[j];
+                --j;
+            }
+            A[j + 1] = key;
+        }
     }
 
-    int *arr = new int[n];
-    srand(time(0));
-    for (int i = 0; i < n; i++) arr[i] = rand() % n;
+    const int THRESHOLD = 30;
+    template <typename E, typename Comp>
+    void quicksortHybrid(E A[], int left, int right) {
+        while (left < right) {
+            if (right - left + 1 <= THRESHOLD) {
+                insertionSort(A, left, right);
+                return;
+            }
+            int pivotIndex = medianOfThreePivot(A, left, right);
+            swap(A, pivotIndex, right);
+            int partitionIndex = partition<E, Comp>(A, left - 1, right + 1, A[right]);
+            swap(A, partitionIndex, right);
 
-    cout << "Sorting performance:\n";
-    measureSortingTime(hybridQuickSort<int, Comparator>, arr, n, "Hybrid Sort", 30);
-    measureSortingTime(iterativeQuickSort<int, Comparator>, arr, n, "Iterative Sort", 30);
+            if (partitionIndex - left < right - partitionIndex) {
+                quicksortHybrid<E, Comp>(A, left, partitionIndex - 1);
+                left = partitionIndex + 1;
+            } else {
+                quicksortHybrid<E, Comp>(A, partitionIndex + 1, right);
+                right = partitionIndex - 1;
+            }
+        }
+    }
 
-    delete[] arr;
-    return 0;
-}
+    template <typename E, typename Comp>
+    void quicksortNonRecursive(E A[], int n) {
+        stack<pair<int, int>> stk;
+        stk.push(make_pair(0, n - 1));
+
+        while (!stk.empty()) {
+            int left = stk.top().first, right = stk.top().second;
+            stk.pop();
+
+            if (right - left + 1 <= THRESHOLD) {
+                insertionSort(A, left, right);
+                continue;
+            }
+
+            int pivotIndex = medianOfThreePivot(A, left, right);
+            swap(A, pivotIndex, right);
+            int partitionIndex = partition<E, Comp>(A, left - 1, right + 1, A[right]);
+            swap(A, partitionIndex, right);
+
+            if (partitionIndex - left < right - partitionIndex) {
+                if (partitionIndex + 1 < right) stk.push(make_pair(partitionIndex + 1, right));
+                right = partitionIndex - 1;
+            } else {
+                if (partitionIndex - 1 > left) stk.push(make_pair(left, partitionIndex - 1));
+                left = partitionIndex + 1;
+            }
+        }
+    }
+
+    void measureSortTime(void (*sortFunction)(int*, int, int), int A[], int n, const string &label) {
+        int* arr = new int[n];
+        copy(A, A + n, arr);
+
+        auto start = chrono::high_resolution_clock::now();
+        sortFunction(arr, 0, n - 1);
+        auto end = chrono::high_resolution_clock::now();
+        cout << label << ": " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
+
+        delete[] arr;
+    }
+
+    void measureSortTimeNonRecursive(void (*sortFunction)(int*, int), int A[], int n, const string &label) {
+        int* arr = new int[n];
+        copy(A, A + n, arr);
+
+        auto start = chrono::high_resolution_clock::now();
+        sortFunction(arr, n);
+        auto end = chrono::high_resolution_clock::now();
+        cout << label << ": " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
+
+        delete[] arr;
+    }
+
+    int main() {
+        int n;
+        cout << "Enter the number of elements (n >= 1000): ";
+        cin >> n;
+
+        if (n < 1000) {
+            cout << "n must be >= 1000.\n";
+            return 1;
+        }
+
+        int* data = new int[n];
+        srand(time(0));
+        for (int i = 0; i < n; i++) {
+            data[i] = rand() % n;
+        }
+
+        cout << "Sorting results:\n";
+        measureSortTime(quicksortOriginal<int, Ascending>, data, n, "Original ");
+        measureSortTime(quicksortMedian3<int, Ascending>, data, n, "(a)");
+        measureSortTime(quicksortHybrid<int, Ascending>, data, n, "(a) + (b)");
+        measureSortTimeNonRecursive(quicksortNonRecursive<int, Ascending>, data, n, "(a) + (b) + (c)");
+
+        delete[] data;
+        return 0;
+    }
